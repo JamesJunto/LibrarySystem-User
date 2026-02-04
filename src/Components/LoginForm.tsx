eaf390c7b42f21d9b41091371e2cdb3bef76bfc5
Chamiruuuu <jamesjunto22@gmail.com>
2026-02-05 00:34:15 +0800

    implement login function

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
+import type { ILogin } from "../interface/ILogin";
+
 export const LoginForm = () => {
+  const { Login } = useLogin();
+
+  const [form, setForm] = useState<ILogin>({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await Login(form)
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-base-200">
     <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 ">
       <legend className="fieldset-legend">Login</legend>
 
       <label className="label">Email</label>
-      <input type="email" className="input" placeholder="Email" />
+      <input type="email" value={form.username} onChange={handleChange} name="username" className="input" placeholder="Username" />
 
       <label className="label">Password</label>
-      <input type="password" className="input" placeholder="Password" />
+      <input type="password" value={form.password} onChange={handleChange} name="password" className="input" placeholder="Password" />
 
-      <button className="btn btn-neutral mt-4">Login</button>
+      <button onClick={handleSubmit} className="btn btn-neutral mt-4">Login</button>
     </fieldset>
     </div>
   );
---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,32 @@
+import type { ILogin } from "../interface/ILogin";
+
+export const useLogin = () => {
+
+  const Login = async (data: ILogin)  => {
+    try {
+      const response = await fetch("http://localhost:8080/Login.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include", 
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+        console.log("Login response:", result);
+
+
+    } catch (err) {
+        console.error("Error submitting form:", err);
+        throw err; 
+    }
+
+  };
+  
+  return { Login };
+};
